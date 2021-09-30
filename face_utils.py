810df642d5f5a3ceacb652f321c109eb056659b7
Mohammad Alhamdan <muhammad.alhamdan@gmail.com>
2021-09-30 01:53:06 -0400

    Fixed when action = 'age'

--- face_utils.py
+++ face_utils.py
@@ -4,7 +4,7 @@ from deepface import DeepFace
 def face_information(image_path, action):
     info = DeepFace.analyze(image_path, actions=[action])
 
-    if action != 'gender':
+    if action not in ['gender', 'age']:
         return max(info[action], key=info[action].get)
     else:
         return info[action]
@@ -12,7 +12,7 @@ def face_information(image_path, action):
 
 def main():
     pass
-    # info = face_information("examples/example2.jpg", 'race')
+    # info = face_information("examples/example2.jpg", 'age')
     # print(info)
 
 
