a5bbbd2b61ee1e65d15321e5cff3e63b997aa007
Mohammad Alhamdan <muhammad.alhamdan@gmail.com>
2021-09-30 23:35:12 -0400

    Face information takes PIL now

--- face_utils.py
+++ face_utils.py
@@ -7,7 +7,8 @@ from matplotlib import cm
 
 # Action = 'race' or 'gender' or 'age' or 'emotion'
 def face_information(image_path, action):
-    info = DeepFace.analyze(image_path, actions=[action])
+    input = np.array(image_path)
+    info = DeepFace.analyze(input, actions=[action])
 
     if action not in ['gender', 'age']:
         return max(info[action], key=info[action].get)
